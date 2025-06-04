20d46616d243ace845c0d68579a6a9af64267942
Lenovo_Youssouf <ytangara2003@gmail.com>
2025-06-04 15:23:37 +0100

    feat: ajout du composant standalone NewCustomerComponent pour la création de nouveaux clients

---
+++ src/app/new-customer/new-customer.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from "@angular/forms";
+import { Customer } from "../model/customer.model";
+import { CustomerService } from "../services/customer.service";
+import { Router } from "@angular/router";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: 'app-new-customer',
+  standalone: true,
+  templateUrl: './new-customer.component.html',
+  styleUrls: ['./new-customer.component.css'],
+  imports: [CommonModule, ReactiveFormsModule]
+})
+export class NewCustomerComponent implements OnInit {
+  newCustomerFormGroup!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private customerService: CustomerService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.newCustomerFormGroup = this.fb.group({
+      name: this.fb.control(null, [Validators.required, Validators.minLength(4)]),
+      email: this.fb.control(null, [Validators.required, Validators.email])
+    });
+  }
+
+  handleSaveCustomer() {
+    let customer: Customer = this.newCustomerFormGroup.value;
+    this.customerService.saveCustomer(customer).subscribe({
+      next: () => {
+        alert("Customer has been successfully saved!");
+        this.router.navigateByUrl("/customers");
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+}
